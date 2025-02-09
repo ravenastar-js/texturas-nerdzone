629de7e6c21c838171883f63f71d3f5080c5aa95
RavenaStar <staralien@staralienbot.com>
2025-02-09 07:46:49 -0300

    🔨 nerdzone.gg

--- mc/js/data.js
+++ mc/js/data.js
@@ -11,13 +11,13 @@ const texturas = [
         link: "https://www.mediafire.com/file/7ddpjvrnijch325/%25C2%25A76RavenaStar_%25C2%25A7bSkyGrid_%25C2%25A7cNerd%25C2%25A7fzone.zip/file",
         icone: "mc/imgs/2.png",
         description: "§b【V1】 §dbit.ly/ravenastar§f & §6Feito para §fminérios/madeira§6, §a@Kevenkbm",
-         v: "1.8.9"
+        v: "1.8.9"
     },
     {
         nome: "§6RavenaStar Basic",
         link: "https://www.planetminecraft.com/texture-pack/v1-ravenastar-basic-armor-durability/",
         icone: "mc/imgs/3.png",
         description: "§b【V1】 §dbit.ly/ravenastar",
-         v: "1.8.9"
+        v: "1.8.9"
     }
 ];
