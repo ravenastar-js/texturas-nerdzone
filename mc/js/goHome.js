4f818addd2f8edb4ce9aef67dc671e64dbe95f87
RavenaStar <ravenastar@proton.me>
2025-02-10 00:49:33 -0300

    🔨 nerdzone.gg

---
+++ mc/js/goHome.js
@@ -0,0 +1,4 @@
+
+document.querySelector('.staff-btn#homeBtn').addEventListener('click', () => {
+    window.location.href = './';
+});
--- mc/js/goStaff.js
+++ mc/js/goStaff.js
@@ -1,7 +1,3 @@
 document.querySelector('.staff-btn#staffBtn').addEventListener('click', () => {
     window.location.href = 'staff';
 });
-
-document.querySelector('.staff-btn#homeBtn').addEventListener('click', () => {
-    window.location.href = '/';
-});
