42bb3e4aa79c2359c51ad218d44343eff13b13f2
RavenaStar <ravenastar@proton.me>
2025-03-12 13:03:12 -0300

    🔨 nerdzone.gg

---
+++ mc/js/cp.js
@@ -0,0 +1,24 @@
+function copyLink(fieldId, button) {
+    const linkField = document.getElementById(fieldId);
+    linkField.select();
+    linkField.setSelectionRange(0, 99999); // Para dispositivos móveis
+    navigator.clipboard.writeText(linkField.value)
+        .then(() => {
+            button.classList.add('show-tooltip');
+            setTimeout(() => {
+                button.classList.remove('show-tooltip');
+            }, 2000); // Tooltip visível por 2 segundos
+        })
+        .catch(err => {
+            console.error('Erro ao copiar o link: ', err);
+        });
+}
+
+function copyToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+}
