3bc0f6fb87ffb52a07de78ef48c6ee49910f8717
RavenaStar <ravenastar@proton.me>
2025-03-15 16:26:49 -0300

    🔨 nerdzone.gg

--- mc/js/texturas.js
+++ mc/js/texturas.js
@@ -63,7 +63,7 @@ function renderItens(itensFiltrados, categoria) {
             const iconURL = getIconURL(username, item.hasMinecraftOriginal);
             let btv = item.platform;
             let bt = "";
-           
+
             if (btv.includes("Discord")) {
                 bt = `<button class="bt-dc" onclick="window.open('${item.link}', '_blank')">
                     <div class="svg-wrapper-1">
@@ -77,16 +77,15 @@ function renderItens(itensFiltrados, categoria) {
                     <span>Discord</span>
                 </button>`;
             } else if (btv.includes("YouTube")) {
-                bt = `<button class="youtube-button" onclick="window.open('${item.link}', '_blank')">
-                    <div class="svg-wrapper-1">
+                bt = `<button class="bt-dc youtube-button" onclick="window.open('${item.link}', '_blank')">
+                  <div class="svg-wrapper-1">
                         <div class="svg-wrapper">
-                            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M0 0h24v24H0z" fill="none"></path>
-                                <path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path>
+                            <svg viewBox="0 0 576 512" fill="white" height="1.6em" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"></path>
                             </svg>
                         </div>
                     </div>
-                    <span>YouTube</span>
+                    <span>Youtube</span>
                 </button>`;
             }
 
